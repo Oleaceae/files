7464986d5ac01bb0034c177347fda72679ed9e5a
Your Name <you@example.com>
2024-11-29 00:57:02 -0500

    work_gen

--- work_gen.py
+++ work_gen.py
@@ -7,22 +7,29 @@ work_dir = f"{home_dir}/DeathStarBench/socialNetwork"
 
 test_duration = 1   # Duration of the test in seconds
 rate_dis = "exp"    # Rate distribution, default to be exponential
-rate_avg = 1000      # Avg rate of requests per sec
+rate_avg = 1      # Avg rate of requests per sec
 thread_num = 1      # Number of threads sending requests
 connection_num = 1  # Number of concurrent connections
 
-lua_script = "./wrk2/scripts/social-network/compose-post.lua" # Lua scripts for request generation
+lua_script = f"{home_dir}/DeathStarBench/socialNetwork/wrk2/scripts/social-network/compose-post.lua" # ABSOLUTE path of Lua scripts for request generation
 url = "http://localhost:8080/wrk2-api/post/compose" # Target URL
 
-client_hostname = [[f'node-{idx}' for idx in range(6, 6+5)]]
+client_nodes= [f'node-{idx}' for idx in range(0, 1)] # ssh of hostnames
 result_node = "node-0"
-workgen_cmd = f"../wrk2/wrk -D {rate_dis} -R {rate_avg} -t {thread_num} -c {connection_num} -d {test_duration} -L -s {lua_script} {url}"
-copy_cmd = f"rsync -rP ./lat*.log {result_node}:/tmp/result"
+result_dir = f"/result/tmp"
+
 # -------------Configuration END--------------
 
-os.system("rm -rf /tmp/result/*")
-os.chdir(work_dir)
-with ThreadingGroup(*client_hostname) as client_grp:
-    client_grp.run(workgen_cmd) # Content saved to lat_{hostname}.log
-    client_grp.run(copy_cmd)    # Copy log to /tmp/result in node-0
-    
+with ThreadingGroup(*client_nodes) as client_grp:
+    # Create and clear result dir
+    client_grp.run(f"mkdir -p {result_dir}")
+    client_grp.run(f"rm -rf {result_dir}/*.log")
+
+    # Generate and copy logs
+    workgen_cmd = f"{work_dir}/../wrk2/wrk -D {rate_dis} -R {rate_avg} -t {thread_num} -c {connection_num} -d {test_duration} -L -s {lua_script} {url}"
+    print(workgen_cmd)
+    client_grp.run(workgen_cmd)  # Content saved to lat_{hostname}.log
+    copy_cmd = f"rsync -P {work_dir}/lat*.log {result_node}:/tmp/result"
+    client_grp.run(copy_cmd)     # Copy log to result_dir in result_node
+
+print(f"Done, result saved to {result_dir} of {result_node}")
