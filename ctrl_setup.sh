5f41fbd7615cbfe9960e18a4225023e9636f4d56
Your Name <you@example.com>
2024-11-29 10:22:38 -0500

    setup update

--- ctrl_setup.sh
+++
@@ -1,9 +0,0 @@
-CONTROLLER_NODE=pc755.emulab.net
-
-./controller_setup.sh \
---username wbcheng \
---private_ssh_key_path "/users/wbcheng/.ssh/id_rsa" \
---controller_node $CONTROLLER_NODE \
---git_email wcheng78@gatech.edu \
---swarm_node_number 6 \
---client_node_number 5
--- setup.sh
+++ setup.sh
@@ -15,7 +15,8 @@ mv jdk-8u241-linux-x64.tar.gz ~/RubbosClient/rubbos
 chmod +x ~/RubbosClient/elba/rubbos/jdk1.8.0_241/bin/java
 
 # Move ctrl_setup.sh to ~
-mv ctrl_setup.sh ..
+sed -i.bak "s/PLACEHOLDER/$CONTROLLER_HOST.$HOST_NAME/g" ./ctrl_setup.sh.template
+mv ctrl_setup.sh.template ../ctrl_setup.sh
 
 # Change to dir ~
 cd ~
@@ -24,7 +25,18 @@ cd ~
 sed -i.bak 's/$your_cloud_lab_username/wbcheng/g' ./config/config.json
 sed -i.bak "s/\$username-six_digit_number/$EXPR_NAME/g" ./config/config.json
 sed -i.bak "s/\$host_ssh_name/$HOST_NAME/g" ./config/config.json
-sed -i.bak "s/PLACEHOLDER/$CONTROLLER_HOST.$HOST_NAME/g" ./ctrl_setup.sh
 
 # Change hardcoded path
 sed -i.bak 's/azhang/wbcheng/g' ./socialNetwork/runtime_files/rubbos.properties_1000
+
+# Change controller_setup.sh
+sed -i.bak "98,105d" controller_setup.sh
+
+# Setup Controller
+./controller_setup.sh \
+--username wbcheng \
+--private_ssh_key_path "/users/wbcheng/.ssh/id_rsa" \
+--controller_node $CONTROLLER_HOST.$HOST_NAME \
+--git_email wcheng78@gatech.edu \
+--swarm_node_number 6 \
+--client_node_number 5
