7d2f76457ba0aae82a978c633b54b7823550ca2c
Your Name <you@example.com>
2025-04-07 15:36:06 -0400

    atmostonce

--- lab/src/dslabs/atmostonce/AMOApplication.java
+++ lab/src/dslabs/atmostonce/AMOApplication.java
@@ -9,6 +9,9 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import java.util.HashMap;
+import dslabs.framework.Address;
+
 @EqualsAndHashCode
 @ToString
 @RequiredArgsConstructor
@@ -16,17 +19,35 @@ public final class AMOApplication<T extends Application> implements Application 
   @Getter @NonNull private final T application;
 
   // Your code here...
+  private HashMap<Address, AMOResult> store = new HashMap<>();
 
   @Override
   public AMOResult execute(Command command) {
     if (!(command instanceof AMOCommand)) {
       throw new IllegalArgumentException();
     }
-
+    
     AMOCommand amoCommand = (AMOCommand) command;
 
+
+
     // Your code here...
-    return null;
+    // Check if it's an old request
+    if (this.alreadyExecuted(amoCommand)) {
+      // System.out.println("Command " + amoCommand.id() + " from client " + amoCommand.address() + " executed");
+      return this.store.get(amoCommand.address());
+    }
+
+    // Execute command
+    AMOResult amoresult =  new AMOResult(
+      this.application.execute(amoCommand.command()),
+      amoCommand.id()
+    );
+      
+      
+    // Mark as executed
+    this.store.put(amoCommand.address(), amoresult);
+    return amoresult;
   }
 
   public Result executeReadOnly(Command command) {
@@ -43,6 +64,10 @@ public final class AMOApplication<T extends Application> implements Application 
 
   public boolean alreadyExecuted(AMOCommand amoCommand) {
     // Your code here...
+    AMOResult save_res = this.store.get(amoCommand.address());
+    if (save_res != null && amoCommand.id() <= save_res.id()) {
+      return true;
+    }
     return false;
   }
 }
--- lab/src/dslabs/atmostonce/AMOCommand.java
+++ lab/src/dslabs/atmostonce/AMOCommand.java
@@ -3,7 +3,12 @@ package dslabs.atmostonce;
 import dslabs.framework.Command;
 import lombok.Data;
 
+import dslabs.framework.Address;
+
 @Data
 public final class AMOCommand implements Command {
   // Your code here...
+  private final Command command;
+  private final int id;
+  private final Address address;
 }
